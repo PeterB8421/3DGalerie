8d8d399297ee9ace3d0a008c57bd8ddd37b0fea3
PeterB8421 <petrbalok@gmail.com>
2019-10-16 14:40:45 +0200

    URL pro localhost

--- gallery/gallery/urls.py
+++ gallery/gallery/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('', include('objectGallery.urls')),
     path('models/', include('objectGallery.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
